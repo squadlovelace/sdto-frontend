810bae96ce2f4d723c21817af896e4e39382a5af
Elias Fernandes <eliasneto0014@gmail.com>
2022-11-23 22:38:52 -0300

    feat(adicionando pós doador): adicionando pós login do doador

---
+++ src/components/sidebar/sidebardoador/index.tsx
@@ -0,0 +1,25 @@
+
+import EditIcon from '@mui/icons-material/Edit';
+import HomeIcon from '@mui/icons-material/Home';
+import { Link } from 'react-router-dom';
+
+import Logo from '../../../assets/logoFormAccess.png';
+import Avatar from '../Avatar';
+import Itemsside from './itemsside';
+
+const SidebarDoador = () => (
+  <section className="bg-white rounded-[7px] border-[1px] border-solid border-[#d0d5dd] px-9 py-8  my-[30px] ml-[30px]  w-[280px]">
+    <Link to="/">
+      <img src={Logo} alt="logo do sdto" />
+    </Link>
+
+    <div className="flex flex-col mt-[120px] gap-4 mb-[570px]">
+      <Itemsside icon={<HomeIcon />} text="Início" />
+      <Itemsside icon={<EditIcon />} text="Editar perfil" />
+      
+    </div>
+    <Avatar initalName="US" nameUser="USER" typeUser="Doador" />
+  </section>
+);
+
+export default SidebarDoador;

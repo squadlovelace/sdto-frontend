67a02104afdc37948427b917a26da6a1193580fa
Elias Fernandes <eliasneto0014@gmail.com>
2022-11-23 22:59:16 -0300

    fix(teste): testando uma feature

--- src/App.tsx
+++ src/App.tsx
@@ -53,6 +53,7 @@ function App() {
          <Route path="/editperfildoador" element={<EditPerfilDoador />} />
          <Route path="/lisrinstitutionsdoador" element={<InstitutionDoador />}
         />
+        
       </Routes>
     </BrowserRouter>
   );
